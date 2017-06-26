8263cecd3bc377000fef71267757607f4f228deb
1216114309zpf <1216114309@qq.com>
2017-06-26 01:12:39 -0700

    modify TopDown.cpp

--- DynamicProgramming/LCS/TopDown.cpp
+++ DynamicProgramming/LCS/TopDown.cpp
@@ -5,12 +5,15 @@
 using namespace std;
 
 int computeLCS(int **,int,int,string,string);
+void PrintSolution(int **,int,int,string,string);
 
 int main()
 {
   string X,Y;
   cout<<"Input string X and Y: "<<endl;
-  cin>>X>>Y;
+  //cin>>X>>Y;
+  X="ABCBDAB";
+  Y="BDCABA";
 
   int sizeX=X.size()+1;
   int sizeY=Y.size()+1;
@@ -21,27 +24,42 @@ int main()
      array[i]=new int[sizeY];
   }
 
-  for(int i=0;i<sizeX;++i){
-     array[i][0]=0;
-  }
+   computeLCS(array,sizeX-1,sizeY-1,X,Y);
+   cout<<"The longest common subsequence of X and Y is: "<<array[sizeX-1][sizeY-1]<<endl;
+   PrintSolution(array,sizeX-1,sizeY-1,X,Y);
+}
 
-  for(int j=0;j<sizeY;++j){
-     array[0][j]=0;
-  }
 
-  for(int i=1;i<sizeX;++i){
-     for(int j=1;j<sizeY;++j){
-        if(X[i-1]==Y[j-1]){
-           array[i][j]=array[i-1][j-1]+1;
+int computeLCS(int **array,int i,int j,string X,string Y){
+    if(i==0 or j==0){
+        array[i][j] = 0;
+        return 0;
+    }
+
+    if(X[i-1]==Y[j-1]){
+           array[i][j]=computeLCS(array,i-1,j-1,X,Y)+1;
         }
-        else if(array[i][j-1]>array[i-1][j]){
-           array[i][j]=array[i][j-1];
+        else if(computeLCS(array,i,j-1,X,Y)>computeLCS(array,i-1,j,X,Y)){
+           array[i][j]=computeLCS(array,i,j-1,X,Y);
         }
         else{
-           array[i][j]=array[i-1][j];
+           array[i][j]=computeLCS(array,i-1,j,X,Y);
         }
-     }
-   }
-
-   cout<<"The longest common subsequence of X and Y is: "<<array[sizeX-1][sizeY-1]<<endl;
+    return array[i][j];
 }
+
+void PrintSolution(int **array,int i,int j,string X, string Y){
+     if(i==0 or j==0)
+        return;
+     if(X[i-1]==Y[j-1]){
+           PrintSolution(array,i-1,j-1,X,Y);
+           cout<<X[i-1]<<" ";
+        }
+        else if(array[i][j-1]>array[i-1][j]){
+           PrintSolution(array,i,j-1,X,Y);
+        }
+        else{
+           PrintSolution(array,i-1,j,X,Y);
+        }
+}
+
